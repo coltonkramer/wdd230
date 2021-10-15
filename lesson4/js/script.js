75f84be60a4221853bb91a2015e3366711b3d3bf
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-10-15 16:19:08 -0400

    style changes

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,4 +1,16 @@
-document.getElementById("copyrightyear").textContent = new Date().getFullYear();
+let today = new Date();
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+let day = days[today.getDay()];
+let date = today.getDate();
+let month = months[today.getMonth()];
+let year = today.getFullYear();
+
+let formattedDate = day + ", " + date + " " + month + " " + year
+
+document.getElementById("current-full-date").textContent = formattedDate;
 
 document.getElementById("lastmodified").innerHTML = document.lastModified;
 
