631b790e0f27be04648ac5988657e4faee797ff3
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-11-06 21:27:36 -0400

    Finished the storm center page

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -39,3 +39,7 @@ if (temp <= 50 && speed > 3){
     let windChill = Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16))  
     document.querySelector('#wind-chill').innerHTML = windChill + "°F"
 }
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
