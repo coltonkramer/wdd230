69b2cf2aa7b81a21bb8ca042a7b5eadb69b4ff29
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-11-16 17:59:28 -0500

    added tge s

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=953336065a7c8c590044bd8c972cc6b6";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=953336065a7c8c590044bd8c972cc6b6";
 fetch(apiURL)
   .then((response) => response.json())
   .then((weatherData) => {
