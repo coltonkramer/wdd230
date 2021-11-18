14265f2f9f11c42bb71115ed3304ec974c07eb9b
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-11-18 17:23:55 -0500

    weather summary is working correctly... Just need to finish the forecast section

---
+++ lesson10/js/script.js
@@ -0,0 +1,38 @@
+let today = new Date();
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+let day = days[today.getDay()];
+let date = today.getDate();
+let month = months[today.getMonth()];
+let year = today.getFullYear();
+
+let formattedDate = day + ", " + date + " " + month + " " + year
+
+document.getElementById("current-full-date").textContent = formattedDate;
+
+document.getElementById("lastmodified").innerHTML = document.lastModified;
+
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+function announcement(){
+        const announce = document.getElementById('announcement');
+        const p = document.createElement('p');
+        const text = 'Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion';
+        p.append(text);
+        announce.appendChild(p);
+
+        announce.style.display = "block" 
+
+}
+if(day == 'Friday'){
+    announcement();
+}
+
+
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,27 +1,69 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=953336065a7c8c590044bd8c972cc6b6";
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=953336065a7c8c590044bd8c972cc6b6";
 fetch(apiURL)
   .then((response) => response.json())
   .then((weatherData) => {
     console.log(weatherData);
 
-    let temp = Math.floor(convertTemp(weatherData)) 
-    let icon = 'https://openweathermap.org/img/w/' + weatherData.weather[0]['icon'] + '.png'
-    display(temp, icon)
+    let description = weatherData.weather[0]["main"];
+    let temp = Math.floor(convertTemp(weatherData.main.temp));
+    let highTemp = Math.floor(convertTemp(weatherData.main.temp_max));
+    let lowTemp = Math.floor(convertTemp(weatherData.main.temp_min));
+
+    let icon =
+      "https://openweathermap.org/img/w/" +
+      weatherData.weather[0]["icon"] +
+      ".png";
+    let windSpeed = weatherData.wind.speed.toFixed(1);
+    let windChill = getWindChill(temp, windSpeed)
+    let humidity = weatherData.main.humidity
+
+    display(description, temp, highTemp, lowTemp, icon, windChill, windSpeed, humidity);
   });
 
-function convertTemp(weatherData){
-    return (weatherData.main.temp - 273.15) * 9/5 + 32
+function convertTemp(temperature) {
+  return ((temperature - 273.15) * 9) / 5 + 32;
 }
 
-function display(temp, icon){
-    let tempSelector = document.querySelector('#current-temp')
-    tempSelector.textContent = temp
+function display(description, temp, highTemp, lowTemp, icon, windChill, windSpeed, humidity) {
+  let descriptionSelector = document.querySelector("#currently");
+  descriptionSelector.textContent = description;
 
-    let linkSelector = document.querySelector('#imagesrc')
-    linkSelector.textContent = icon
+  let tempSelector = document.querySelector("#temp");
+  tempSelector.textContent = temp + '°';
 
-    let imgSelector = document.querySelector('#icon')
-    imgSelector.setAttribute('src', icon)
-    imgSelector.setAttribute('alt', 'Descriptive weather icon')
+  let highLowTempSelector = document.querySelector("#high-low-temp");
+  highLowTempSelector.textContent = ' ↑' + highTemp + '°' + '/' + ' ↓' + lowTemp + '°';
+
+
+  let chillSelector = document.querySelector("#wind-chill");
+  if (windChill === 'N/A'){
+    chillSelector.textContent = windChill;
+  }
+  else{
+    chillSelector.textContent = windChill + '°F';
+  }
+
+  let speedSelector = document.querySelector('#wind-speed');
+  speedSelector.textContent = windSpeed + 'MPH';
+
+  let humidSelector = document.querySelector("#humidity");
+  humidSelector.textContent = humidity + '%';
+
+  let linkSelector = document.querySelector("#imagesrc");
+  linkSelector.textContent = icon;
+
+
+  let imgSelector = document.querySelector("#icon");
+  imgSelector.setAttribute("src", icon);
+  imgSelector.setAttribute("alt", "Descriptive weather icon");
 }
 
+function getWindChill(temp, speed){
+  if (temp <= 50 && speed > 3) {
+    return Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16));
+  }
+  else {
+    return 'N/A'
+  }
+}
