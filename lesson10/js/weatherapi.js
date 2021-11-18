e0598beb9bb143d851851e255a6bad71acba71cc
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-11-18 17:31:51 -0500

    moved a funciton in js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -21,49 +21,50 @@ fetch(apiURL)
     display(description, temp, highTemp, lowTemp, icon, windChill, windSpeed, humidity);
   });
 
-function convertTemp(temperature) {
-  return ((temperature - 273.15) * 9) / 5 + 32;
-}
-
-function display(description, temp, highTemp, lowTemp, icon, windChill, windSpeed, humidity) {
-  let descriptionSelector = document.querySelector("#currently");
-  descriptionSelector.textContent = description;
-
-  let tempSelector = document.querySelector("#temp");
-  tempSelector.textContent = temp + '°';
-
-  let highLowTempSelector = document.querySelector("#high-low-temp");
-  highLowTempSelector.textContent = ' ↑' + highTemp + '°' + '/' + ' ↓' + lowTemp + '°';
-
-
-  let chillSelector = document.querySelector("#wind-chill");
-  if (windChill === 'N/A'){
-    chillSelector.textContent = windChill;
+  function display(description, temp, highTemp, lowTemp, icon, windChill, windSpeed, humidity) {
+    let descriptionSelector = document.querySelector("#currently");
+    descriptionSelector.textContent = description;
+    
+    let tempSelector = document.querySelector("#temp");
+    tempSelector.textContent = temp + '°';
+    
+    let highLowTempSelector = document.querySelector("#high-low-temp");
+    highLowTempSelector.textContent = ' ↑' + highTemp + '°' + '/' + ' ↓' + lowTemp + '°';
+    
+    
+    let chillSelector = document.querySelector("#wind-chill");
+    if (windChill === 'N/A'){
+      chillSelector.textContent = windChill;
+    }
+    else{
+      chillSelector.textContent = windChill + '°F';
+    }
+    
+    let speedSelector = document.querySelector('#wind-speed');
+    speedSelector.textContent = windSpeed + 'MPH';
+    
+    let humidSelector = document.querySelector("#humidity");
+    humidSelector.textContent = humidity + '%';
+    
+    let linkSelector = document.querySelector("#imagesrc");
+    linkSelector.textContent = icon;
+    
+    
+    let imgSelector = document.querySelector("#icon");
+    imgSelector.setAttribute("src", icon);
+    imgSelector.setAttribute("alt", "Descriptive weather icon");
   }
-  else{
-    chillSelector.textContent = windChill + '°F';
+
+  function convertTemp(temperature) {
+    return ((temperature - 273.15) * 9) / 5 + 32;
   }
-
-  let speedSelector = document.querySelector('#wind-speed');
-  speedSelector.textContent = windSpeed + 'MPH';
-
-  let humidSelector = document.querySelector("#humidity");
-  humidSelector.textContent = humidity + '%';
-
-  let linkSelector = document.querySelector("#imagesrc");
-  linkSelector.textContent = icon;
-
-
-  let imgSelector = document.querySelector("#icon");
-  imgSelector.setAttribute("src", icon);
-  imgSelector.setAttribute("alt", "Descriptive weather icon");
-}
-
-function getWindChill(temp, speed){
-  if (temp <= 50 && speed > 3) {
-    return Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16));
+  
+  function getWindChill(temp, speed){
+    if (temp <= 50 && speed > 3) {
+      return Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16));
+    }
+    else {
+      return 'N/A'
+    }
   }
-  else {
-    return 'N/A'
-  }
-}
+  
