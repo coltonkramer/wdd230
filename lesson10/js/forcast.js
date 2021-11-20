cdebf5d89bd3b0bad06fea2d2b7bd031ee4afafb
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2021-11-20 13:40:30 -0500

    finished this weeks api assignment

--- lesson10/js/forcast.js
+++ lesson10/js/forcast.js
@@ -6,14 +6,39 @@ fetch(apiURL1)
 
   const noon =  weatherData.list.filter(x => x.dt_txt.includes('18:00:00'))
   forecast(noon)
+  console.log(noon)
 })
 
 function forecast(noon) {
     noon.forEach((i,j) => document.querySelector('#temp' + j).textContent = i.main.temp + '°F')
+    noon.forEach((i,j) => document.querySelector('#day' + j).textContent = getDay(new Date(i.dt_txt).getDay()))
+    noon.forEach((i,j) => document.querySelector('#icon' + j).setAttribute('src', getIcon(i.weather[0].icon)))
+
+}
+
+function getDay(day){
+  switch(day){
+    case 0:
+      return 'Sunday'
+    case 1:
+      return 'Monday'
+    case 2:
+      return 'Tuesday'
+    case 3:
+      return 'Wednesday'
+    case 4:
+      return 'Thursday'
+    case 5:
+      return 'Friday'
+    case 6:
+      return 'Saturday'
+  }
+}
+
+function getIcon(icon){
+  return "https://openweathermap.org/img/w/" + icon + ".png"
+
 }
 
 
 
-
-
-// let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0]["icon"] +".png";
